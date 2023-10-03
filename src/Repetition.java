7ea284d040cfbaa46b647a7c4cb60415adc53d7c
lvcky-gg <matadorcrab@gmail.com>
2023-10-02 20:14:24 -0500

    for loop

--- src/Repetition.java
+++ src/Repetition.java
@@ -11,5 +11,9 @@ public class Repetition {
             System.out.println(countTwo);
             countTwo--;
         }while(countTwo > 0);
+
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
